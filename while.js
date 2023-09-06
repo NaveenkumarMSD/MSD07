933eafcfef43c9181bc4b2215cf96cc0b9245fae
NaveenkumarMSD <139426587+NaveenkumarMSD@users.noreply.github.com>
2023-09-06 16:40:38 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,26 @@
+var age=26;
+let name= "Naveen MSD";
+
+if (age<25){
+  console.log(" eligible for jobs");
+}else if(age>25){
+    console.log("not eligible for jobs");
+}else{
+console.log("last chance for jobs");
+}
+
+let i = 0;
+for(i=0;i<name.length; i++);
+{
+  console.log(name[i]);
+}
+// let g = 0;
+// while (g < age && age <= 30) {
+//     console.log(name);
+//     g++;
+// }
+let j = 0;
+do {
+    console.log(name);
+    j++;
+} while (j === 1 || (j % 2 === 0 && age % 2 === 0))
